00971f74a401f808b24a49e09860dc7100973ef7
ayushkumarsingh121 <ayushkumarsinghrajput121@gmai.com>
2025-01-05 16:12:14 +0530

    first evalution i solve 4Q/7Q

---
+++ Evaluation 1/Q5_Employee_Grouping_and_Sorting.js
@@ -0,0 +1,62 @@
+// 5. Employee Grouping and Sorting
+// You have been provided with an array of employee objects. Each employee object contains the following properties: name, age, salary, and department. Your task is to create a function groupByDepartment that takes this array of employees as input and returns an object where employees are grouped by their departments. Additionally, within each department, the employees should be sorted in ascending order based on their ages.
+
+// Write a function groupByDepartment that performs the following tasks:
+
+// Group the employees by their departments.
+// Sort the employees within each department based on their ages in ascending order.
+// Return an object where keys are department names and values are arrays containing employees sorted by age.
+// Ensure that the function handles the case where there are no employees in a particular department.
+
+// Example:
+
+// const employees = [
+//  { name: "Alice", age: 30, salary: 50000, department: "HR" },
+//  { name: "Bob", age: 35, salary: 60000, department: "Finance" },
+//  { name: "Charlie", age: 28, salary: 55000, department: "HR" },
+//  { name: "David", age: 40, salary: 70000, department: "IT" },
+//  { name: "Eve", age: 32, salary: 65000, department: "Finance" },
+// ];
+
+// console.log(groupByDepartment(employees));
+// Output:
+
+// {
+//  HR: [
+//     { name: 'Charlie', age: 28, salary: 55000 },
+//     { name: 'Alice', age: 30, salary: 50000 }
+//   ],
+//   Finance: [
+//     { name: 'Eve', age: 32, salary: 65000 },
+//     { name: 'Bob', age: 35, salary: 60000 }
+//   ],
+//   IT: [
+//     { name: 'David', age: 40, salary: 70000 }
+//   ]
+// }
+
+
+const employees = [
+  { name: "Alice", age: 30, salary: 50000, department: "HR" },
+  { name: "Bob", age: 35, salary: 60000, department: "Finance" },
+  { name: "Charlie", age: 28, salary: 55000, department: "HR" },
+  { name: "David", age: 40, salary: 70000, department: "IT" },
+  { name: "Eve", age: 32, salary: 65000, department: "Finance" },
+ ];
+ 
+
+ let groupByDepartment = (employees) => {
+  return employees.reduce((acc, emp) => {
+    if(!acc[emp.department])
+      acc[emp.department] = [];
+
+    acc[emp.department].push(emp);
+    acc[emp.department].sort((a,b) => a.age - b.age)
+    return acc
+  }, {})
+ }
+
+
+
+ console.log(groupByDepartment(employees));
+ 
